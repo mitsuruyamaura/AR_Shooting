ae5e0d67cacc6e917464711e2bb0eaa918c2e3eb
mitsuru_yamaura <komayou326@gmail.com>
2021-03-06 14:10:09 +0900

    バレット作成し、プレファブ化。Bullet.cs、BulletGenerator.cs を作成。 画面をタップしたらバレットを生成し、タップした方向へバレットを発射する処理を追加。 正規化せずに高い位置でタップするとより遠くに飛ぶようにしてある。

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    public float shotSpeed;
+
+    public void Shot(Vector3 direction) {
+        GetComponent<Rigidbody>().AddForce(direction * shotSpeed);
+
+        Destroy(gameObject, 3.0f);
+    }
+}
