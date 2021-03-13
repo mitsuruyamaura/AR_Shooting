ae7e3ec94c870b61aea05e3f6afe2700da267405
mitsuru_yamaura <komayou326@gmail.com>
2021-03-13 13:50:47 +0900

    ARSessionOrigin(ARCamera コンポーネント付)を作成。ARPlaneManager コンポーネント、ARRaycastManager コンポーネントを追加。 ARManager.cs 作成。ビルドの設定をAR用に変更。ビルドしてデバッグ開始。

---
+++ Assets/Scripts/ARManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objPrefab = null;
+
+    private GameObject obj;
+
+    private ARRaycastManager raycastManager;
+
+    private List<ARRaycastHit> raycastHitList = new List<ARRaycastHit>();
+
+
+    void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase != TouchPhase.Ended) {
+                return;
+            }
+
+            if (obj == null) {
+                if (raycastManager.Raycast(touch.position, raycastHitList, TrackableType.Planes)) {
+                    Debug.Log("Raycast成功");
+
+                    ///Vector3 posY = new Vector3(raycastHitList[0].pose.position.x, raycastHitList[0].pose.position.y + 0.3f, raycastHitList[0].pose.position.z + 1f);
+
+
+                    obj = Instantiate(objPrefab, raycastHitList[0].pose.position, Quaternion.identity);
+                } else {
+                    Debug.Log("RayCast失敗");
+                }
+            } 
+        }
+    }
+}
