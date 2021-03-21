f350715bf700118a08d0bc88965ba42f7fc7bc03
mitsuru_yamaura <komayou326@gmail.com>
2021-03-21 12:38:03 +0900

    生成されたアヒルを空中で一時停止する制御を追加。バレット(魚)をぶつけると破壊する処理を追加。 アヒルのアニメーション制御を追加。バレットの発射位置やアヒルの生成位置は要調整。 実機テスト、問題なし。

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -5,7 +5,31 @@ using DG.Tweening;
 
 public class AnimalController : MonoBehaviour
 {
+    private Animator anim;
+    private Tween tween;
+
     public void MoveAnimal() {
-        transform.DOMoveY(10, 3.0f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("jump");
+        tween = transform.DOMoveY(2.5f, 3.0f).SetEase(Ease.InBack)
+            .OnComplete(() => {
+                transform.DOMoveY(0, 3.0f).SetEase(Ease.InBack)
+                    .OnComplete(() => {
+                        Destroy(gameObject);
+                    });
+            });
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Bullet")) {
+            tween.Kill();
+
+            anim.ResetTrigger("jump");
+            anim.SetTrigger("stun");
+
+            Destroy(other.gameObject);
+
+            Destroy(gameObject, 1.0f);
+        }
     }
 }
