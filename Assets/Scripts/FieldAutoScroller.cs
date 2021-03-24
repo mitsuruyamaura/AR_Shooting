0a5263cd3dc413794bb09225eae38f6ebe1211e4
mitsuru_yamaura <komayou326@gmail.com>
2021-03-24 19:18:09 +0900

    フィールド再構成。カメラの自動移動処理を追加。ARでの実機テスト完了。

---
+++ Assets/Scripts/FieldAutoScroller.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq;
+
+public class FieldAutoScroller : MonoBehaviour
+{
+    [System.Serializable]
+    public class PathData {
+        public float scrollTime;
+        public Transform pathTran;
+    }
+
+    [SerializeField]
+    private List<PathData> pathDatasList = new List<PathData>();
+
+    Tween tween;
+
+    bool isPause;
+
+    IEnumerator Start() {
+
+        Vector3[] paths = pathDatasList.Select(x => x.pathTran.position).ToArray();
+        float totalTime = pathDatasList.Select(x => x.scrollTime).Sum();
+
+        Debug.Log(totalTime);
+        tween = transform.DOPath(paths, totalTime);
+
+        yield return null;
+    }
+
+    private void Update() {
+
+        // àÍéûí‚é~Ç∆çƒäJèàóù
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (isPause) {
+                transform.DOPlay();
+            } else {
+                transform.DOPause();
+            }
+            isPause = !isPause;
+        }
+    }
+}
